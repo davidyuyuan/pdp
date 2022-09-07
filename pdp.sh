1892db98954ddb32c7125c74411a500b569754ad
davidyuyuan <davidyuan@ebi.ac.uk>
2022-09-07 10:09:12 +0100

    Get pathogen analysis only

--- pdp.sh
+++ pdp.sh
@@ -14,17 +14,17 @@ gsutil ls -p "${project_id}" "gs://${dataset_name}" || gsutil mb -p "${project_i
 bq --project_id="${project_id}" show "${dataset_name}" || bq --location="${location}" mk --dataset "${project_id}:${dataset_name}"
 
 echo ""
-echo "** Updating ${dataset_name}.all_analyses table. **"
+echo "** Updating ${dataset_name}.pathogen_analysis table. **"
 
 # https://www.ebi.ac.uk/ena/portal/api/search?fields=all&result=analysis&limit=10&fields=all
 curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=analysis&fields=all&format=tsv&limit=10' \
   "https://www.ebi.ac.uk/ena/portal/api/search" > "${output_dir}/all_fields.tsv"
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=analysis&fields=analysis_accession%2Cstudy_accession%2Csubmitted_bytes%2Canalysis_type%2Csample_accession%2Crun_ref%2Cscientific_name%2Clast_updated%2Canalysis_date%2Csubmitted_ftp%2Csubmitted_aspera%2Csubmitted_galaxy&format=tsv&limit=0' \
-  "https://www.ebi.ac.uk/ena/portal/api/search" > "${output_dir}/all_analyses.tsv"
-gsutil -m cp "${output_dir}/all_analyses.tsv" "gs://${dataset_name}/all_analyses.tsv" && \
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=analysis&dataPortal=pathogen&fields=analysis_accession%2Cstudy_accession%2Csubmitted_bytes%2Canalysis_type%2Csample_accession%2Crun_ref%2Cscientific_name%2Clast_updated%2Canalysis_date%2Csubmitted_ftp%2Csubmitted_aspera%2Csubmitted_galaxy&format=tsv&limit=0' \
+  "https://www.ebi.ac.uk/ena/portal/api/search" > "${output_dir}/pathogen_analysis.tsv"
+gsutil -m cp "${output_dir}/pathogen_analysis.tsv" "gs://${dataset_name}/pathogen_analysis.tsv" && \
   bq --project_id="${project_id}" load --source_format=CSV --replace=true --skip_leading_rows=1 --field_delimiter=tab \
-  --autodetect --max_bad_records=0 "${dataset_name}.all_analyses" "gs://${dataset_name}/all_analyses.tsv" \
+  --autodetect --max_bad_records=0 "${dataset_name}.pathogen_analysis" "gs://${dataset_name}/pathogen_analysis.tsv" \
   "analysis_accession:STRING,study_accession:STRING,submitted_bytes:STRING,analysis_type:STRING,sample_accession:STRING,run_ref:STRING,scientific_name:STRING,last_updated:DATE,analysis_date:DATE"
 
-#curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=analysis&fields=analysis_accession%2Cstudy_accession%2Csubmitted_bytes%2Canalysis_type%2Csample_accession%2Crun_ref%2Cscientific_name%2Clast_updated%2Canalysis_date%2Csubmitted_ftp%2Csubmitted_aspera%2Csubmitted_galaxy&format=tsv&limit=0' \
-#  "https://www.ebi.ac.uk/ena/portal/api/search" > "${output_dir}/all_analyses_ftp.tsv"
+#curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=analysis&fields=analysis_accession%2Cstudy_accession%2Csubmitted_bytes%2Canalysis_type%2Csample_accession%2Crun_ref%2Cscientific_name%2Clast_updated%2Canalysis_date&format=tsv&limit=0' \
+#  "https://www.ebi.ac.uk/ena/portal/api/search" > "${output_dir}/all_analyses.tsv"
