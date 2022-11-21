e952e6f142743b0b91de18eb8a2aae1653503a75
davidyuyuan <davidyuan@ebi.ac.uk>
2022-11-21 18:59:26 +0000

    Allow 100 bad records for analysis and 20000 bad records for run.

--- pdp.sh
+++ pdp.sh
@@ -22,12 +22,12 @@ cp -f "${run_dump}" "${output_dir}/run_dump.tsv.gz" && \
   gunzip -kf "${output_dir}/run_dump.tsv.gz" && \
   gsutil -m cp "${output_dir}/run_dump.tsv" "gs://${dataset_name}/run_dump.tsv" && \
   bq --project_id="${project_id}" load --source_format=CSV --replace=true --skip_leading_rows=1 --field_delimiter='\t' \
-  --autodetect --max_bad_records=2000 "${dataset_name}.run_dump" "gs://${dataset_name}/run_dump.tsv"
+  --autodetect --max_bad_records=20000 "${dataset_name}.run_dump" "gs://${dataset_name}/run_dump.tsv"
 
 # analysis_dump
-echo "Creating analysis_dump..."
-cp -f "${analysis_dump}" "${output_dir}/analysis_dump.tsv.gz" && \
-  gunzip -kf "${output_dir}/analysis_dump.tsv.gz" && \
-  gsutil -m cp "${output_dir}/analysis_dump.tsv" "gs://${dataset_name}/analysis_dump.tsv" && \
-  bq --project_id="${project_id}" load --source_format=CSV --replace=true --skip_leading_rows=1 --field_delimiter='\t' \
-  --autodetect --max_bad_records=100 "${dataset_name}.analysis_dump" "gs://${dataset_name}/analysis_dump.tsv"
+#echo "Creating analysis_dump..."
+#cp -f "${analysis_dump}" "${output_dir}/analysis_dump.tsv.gz" && \
+#  gunzip -kf "${output_dir}/analysis_dump.tsv.gz" && \
+#  gsutil -m cp "${output_dir}/analysis_dump.tsv" "gs://${dataset_name}/analysis_dump.tsv" && \
+#  bq --project_id="${project_id}" load --source_format=CSV --replace=true --skip_leading_rows=1 --field_delimiter='\t' \
+#  --autodetect --max_bad_records=100 "${dataset_name}.analysis_dump" "gs://${dataset_name}/analysis_dump.tsv"
