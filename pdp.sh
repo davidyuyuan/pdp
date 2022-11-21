758a3b34cf2507b25cf615b224957dd0c3ad13dc
davidyuyuan <davidyuan@ebi.ac.uk>
2022-11-21 17:57:42 +0000

    Change from CSV to TSV

--- pdp.sh
+++ pdp.sh
@@ -3,8 +3,8 @@
 dataset_name=${1:-'datahub_metadata_dump'}
 project_id=${2:-'prj-int-dev-covid19-nf-gls'}
 location=${3:-'europe-west2'}
-analysis_dump=${4:-'/nfs/production/tburdett/ena/test/data_file_dump/analysis_dump.csv.gz'}
-run_dump=${5:-'/nfs/production/tburdett/ena/test/data_file_dump/run_dump.csv.gz'}
+analysis_dump=${4:-'/nfs/production/tburdett/ena/test/data_file_dump/analysis_dump.tsv.gz'}
+run_dump=${5:-'/nfs/production/tburdett/ena/test/data_file_dump/run_dump.tsv.gz'}
 
 # DIR where the current script resides
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -21,7 +21,7 @@ echo "Creating run_dump..."
 cp -f "${run_dump}" "${output_dir}/run_dump.csv.gz" && \
   gunzip -kf "${output_dir}/run_dump.csv.gz" && \
   gsutil -m cp "${output_dir}/run_dump.csv" "gs://${dataset_name}/run_dump.csv" && \
-  bq --project_id="${project_id}" load --source_format=CSV --replace=true --skip_leading_rows=1 --field_delimiter=',' \
+  bq --project_id="${project_id}" load --source_format=CSV --replace=true --skip_leading_rows=1 --field_delimiter='\t' \
   --autodetect --max_bad_records=100 "${dataset_name}.run_dump" "gs://${dataset_name}/run_dump.csv"
 
 # analysis_dump
@@ -29,5 +29,5 @@ echo "Creating analysis_dump..."
 cp -f "${analysis_dump}" "${output_dir}/analysis_dump.csv.gz" && \
   gunzip -kf "${output_dir}/analysis_dump.csv.gz" && \
   gsutil -m cp "${output_dir}/analysis_dump.csv" "gs://${dataset_name}/analysis_dump.csv" && \
-  bq --project_id="${project_id}" load --source_format=CSV --replace=true --skip_leading_rows=1 --field_delimiter=',' \
+  bq --project_id="${project_id}" load --source_format=CSV --replace=true --skip_leading_rows=1 --field_delimiter='\t' \
   --autodetect --max_bad_records=10 "${dataset_name}.analysis_dump" "gs://${dataset_name}/analysis_dump.csv"
