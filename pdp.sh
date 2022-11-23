24874d449335303670886ca6857005078ad6f9ea
davidyuyuan <davidyuan@ebi.ac.uk>
2022-11-23 19:59:30 +0000

    Try to load quoted CSV.

--- pdp.sh
+++ pdp.sh
@@ -3,8 +3,8 @@
 dataset_name=${1:-'datahub_metadata_dump'}
 project_id=${2:-'prj-int-dev-covid19-nf-gls'}
 location=${3:-'europe-west2'}
-analysis_dump=${4:-'/nfs/production/tburdett/ena/test/data_file_dump/analysis_dump.tsv.gz'}
-run_dump=${5:-'/nfs/production/tburdett/ena/test/data_file_dump/run_dump.tsv.gz'}
+analysis_dump=${4:-'/nfs/production/tburdett/ena/test/data_file_dump/analysis_dump.csv.gz'}
+run_dump=${5:-'/nfs/production/tburdett/ena/test/data_file_dump/run_dump.csv.gz'}
 
 # DIR where the current script resides
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -18,20 +18,20 @@ output_dir="${DIR}/results"; mkdir -p "${output_dir}"
 
 # run_dump
 echo "Creating run_dump..."
-cp -f "${run_dump}" "${output_dir}/run_dump.tsv.gz" && \
-  gunzip -kf "${output_dir}/run_dump.tsv.gz" && \
-  gsutil -m cp "${output_dir}/run_dump.tsv" "gs://${dataset_name}/run_dump.tsv" && \
-  bq --project_id="${project_id}" load --source_format=CSV --replace=true --skip_leading_rows=1 --field_delimiter='\t' \
-  --autodetect --max_bad_records=10 "${dataset_name}.run_dump" "gs://${dataset_name}/run_dump.tsv" \
+cp -f "${run_dump}" "${output_dir}/run_dump.csv.gz" && \
+  gunzip -kf "${output_dir}/run_dump.csv.gz" && \
+  gsutil -m cp "${output_dir}/run_dump.csv" "gs://${dataset_name}/run_dump.csv" && \
+  bq --project_id="${project_id}" load --source_format=CSV --replace=true --skip_leading_rows=1 --field_delimiter=',' \
+  --autodetect --max_bad_records=10 "${dataset_name}.run_dump" "gs://${dataset_name}/run_dump.csv" \
   "run_accession:STRING,study_accession:STRING,submitted_bytes:INTEGER,scientific_name:STRING,last_updated:DATE,submitted_format:STRING,submitted_md5:STRING,submitted_path:STRING,data_file_role:STRING,submission_account_id:STRING,tax_id:STRING,run_first_created:DATE,fire_oid:STRING"
 
 # analysis_dump
 echo "Creating analysis_dump..."
-cp -f "${analysis_dump}" "${output_dir}/analysis_dump.tsv.gz" && \
-  gunzip -kf "${output_dir}/analysis_dump.tsv.gz" && \
-  gsutil -m cp "${output_dir}/analysis_dump.tsv" "gs://${dataset_name}/analysis_dump.tsv" && \
-  bq --project_id="${project_id}" load --source_format=CSV --replace=true --skip_leading_rows=1 --field_delimiter='\t' \
-  --autodetect --max_bad_records=10 "${dataset_name}.analysis_dump" "gs://${dataset_name}/analysis_dump.tsv" \
+cp -f "${analysis_dump}" "${output_dir}/analysis_dump.csv.gz" && \
+  gunzip -kf "${output_dir}/analysis_dump.csv.gz" && \
+  gsutil -m cp "${output_dir}/analysis_dump.csv" "gs://${dataset_name}/analysis_dump.csv" && \
+  bq --project_id="${project_id}" load --source_format=CSV --replace=true --skip_leading_rows=1 --field_delimiter=',' \
+  --autodetect --max_bad_records=10 "${dataset_name}.analysis_dump" "gs://${dataset_name}/analysis_dump.csv" \
   "analysis_accession:STRING,study_accession:STRING,submitted_bytes:INTEGER,analysis_type:STRING,scientific_name:STRING,last_updated:DATE,submitted_format:STRING,submitted_md5:STRING,submitted_path:STRING,data_file_role:STRING,submission_account_id:STRING,tax_id:STRING,analysis_sub_type:STRING,analysis_first_created:DATE,fire_oid:STRING"
 
 # Unique?
